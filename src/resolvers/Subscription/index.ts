7c34d25c9c92e9d4b2efca3a9b731d410239bad0
Bruno A. Caceres <brunocaceres@hotmail.com>
2020-04-12 08:00:43 -0400

    Implementação do filtro do bodyTemperature na Subscription.

--- src/resolvers/Subscription/index.ts
+++ src/resolvers/Subscription/index.ts
@@ -1,13 +1,22 @@
-import { Sensor, SubscriptionResolver } from '../../types'
+import { Resolver, Sensor, SubscriptionResolver, SubscriptionArgs, SubscriptionPayload } from '../../types'
+import { withFilter } from 'graphql-yoga'
+
+const bodyTemperatureSubscribeFn: Resolver<SubscriptionArgs> = (_, args, ctx) => {
+  const { mutationIn } = args.where
+  const { pubsub } = ctx
+  const channels = mutationIn.map(m => `BODYTEMPERATURE_${m}`)
+  return pubsub.asyncIterator(channels)
+}
+
+const bodyTemperatureFilternFn: Resolver<SubscriptionArgs, SubscriptionPayload<Sensor>> = (payload, args, ctx) => {
+  return payload.node.value > 36
+}
 
 const  BodyTemperature: SubscriptionResolver<Sensor> = {
-  subscribe: (_, args, ctx) => {
-    const { mutationIn } = args.where
-    const { pubsub } = ctx
-    const channels = mutationIn.map(m => `BODYTEMPERATURE_${m}`)
-    return pubsub.asyncIterator(channels)
-  },
-  resolve: payload => payload,
+  subscribe: withFilter(bodyTemperatureSubscribeFn, bodyTemperatureFilternFn),
+  resolve: payload => {
+    return payload
+  }
 }
 
 export default {
