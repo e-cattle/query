dd4c970bfac38f3a23b319c021e9c997c7eb4fa0
Bruno A. Caceres <brunoacaceres@gmail.com>
2020-06-02 09:07:08 -0400

    Ajuste variável REDIS_HOST.

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -2,6 +2,6 @@ const { NODE_ENV = 'development' } = process.env
 const settings = require('./' + NODE_ENV + '.json')
 
 const MONGO_URI = settings.db
-const REDIS_HOST = settings.redis
+const { REDIS_HOST = 'localhost' } = settings.redis
 
 export { MONGO_URI, REDIS_HOST }
