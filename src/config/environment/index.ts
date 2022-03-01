5236a661da2cea169f6d2042979f637040da5378
Ygo Brito <ygo1992@gmail.com>
2022-03-01 10:46:14 -0400

    Add DB_CLOUD e REDIS_CLOUD para reutilizar o query no ambiente cloud.

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -1,7 +1,7 @@
 const { NODE_ENV = 'development' } = process.env
 const settings = require('./' + NODE_ENV + '.json')
 
-const MONGO_URI = settings.db
-const REDIS_HOST = settings.redis
+const MONGO_URI = process.env.DB_CLOUD || settings.db
+const REDIS_HOST = process.env.REDIS_CLOUD || settings.redis
 
 export { MONGO_URI, REDIS_HOST }
